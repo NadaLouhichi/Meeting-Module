3672bc98ac4ffc9a2b030079f044aa9e227aeed8
FaroukBenhaj <mohamedfarouk.benhajamor@esprit.tn>
2025-02-22 00:29:33 +0100

    First commit Front

---
+++ Frontend/FrontOffice_ConstructionIQ/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
